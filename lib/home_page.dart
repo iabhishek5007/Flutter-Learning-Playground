2f0fda7e69dc2b27c2437b7d12411677df6f11fe
Abhishek Kumar <iabhishek5007@gmail.com>
2023-05-21 16:43:13 +0530

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// This class represents the home page of the app
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // App bar that displays the app name
+      appBar: AppBar(
+        title: const Text("My App"),
+      ),
+      // The main body of the home page, which is a centered container with a welcome message
+      body: const Center(
+        child: Text("Welcome To Flutter"),
+      ),
+      // A drawer that can be opened from the side to display extra options or information
+      drawer: const Drawer(),
+    );
+  }
+}

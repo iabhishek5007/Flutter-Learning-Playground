ab51fdc1f7c1154c029b1c56606e35c046a09ef4
Abhishek Kumar <iabhishek5007@gmail.com>
2023-05-22 22:16:52 +0530

    - Changes LoginPage from Stateless to StateFulWidget
    - Added a new functionality to the login page by creating a clickable login button that changes to an icon when clicked.
    
    - Animated the button's width and border radius to give it a polished look. Also added a delay before navigating to the home screen.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first/utils/routes.dart';
+
+import '../utils/routes.dart';
 
 // Define a LoginPage class that extends StatelessWidget
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   // Define a constructor that calls the constructor of the parent class with a named parameter
   const LoginPage({Key? key}) : super(key: key);
 
+  /// * Explanation:
+  ///    -  @override  is a Dart annotation that indicates that this method overrides a method from a superclass or interface.
+  ///   -  State<LoginPage>  specifies the type of state object that will be created for the LoginPage widget.
+  ///    -  createState()  is a method that creates and returns a new state object for the widget.
+  ///    -  =>  is a shorthand notation that replaces the  return  keyword.
+  ///    -  _LoginPageState()  is the class that represents the state of the LoginPage widget.
+  ///    The rewritten code enclosed within
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   // Override the build method to build the widget tree for this LoginPage
-// This method builds the login screen UI
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -26,9 +42,9 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             // Display the welcome text
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             // Add some spacing
             const SizedBox(
@@ -46,6 +62,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter UserName",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   // password input
                   TextFormField(
@@ -59,16 +79,58 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
-                  //submit button
-                  ElevatedButton(
-                    onPressed: () {
-                      //// Navigate to the home screen using the named route
+                  /**
+                   * This code uses an InkWell widget to create a clickable button with text that changes to an icon when clicked.
+                   * The AnimatedContainer animates changes to the button's width and border radius when clicked.
+                   */
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      // waits for 1 second
+                      await Future.delayed(const Duration(seconds: 1));
+                      // navigates to home
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    style:
-                        TextButton.styleFrom(minimumSize: const Size(150, 50)),
-                    child: const Text("Login"),
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      // ternary operator to change width based on changeButton value
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // shape:
+                          //     changeButton ? BoxShape.circle : BoxShape.rectangle,
+                          borderRadius:
+                              // ternary operator to change border radius based on changeButton value
+                              BorderRadius.circular(changeButton ? 50 : 8)),
+                      // ternary operator to change child based on changeButton value
+                      child: changeButton
+                          ? const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                    ),
                   )
+                  //submit button
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     //// Navigate to the home screen using the named route
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   style:
+                  //       TextButton.styleFrom(minimumSize: const Size(150, 50)),
+                  //   child: const Text("Login"),
+                  // )
                 ],
               ),
             )
